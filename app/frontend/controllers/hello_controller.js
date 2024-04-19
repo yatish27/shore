1b19f0943e48bd55d28334b5f91061f0b09b6c76
Yatish Mehta <yatish27@users.noreply.github.com>
2024-04-19 08:02:14 -0700

    Fixed Title and Subtitle for homepage

--- app/frontend/controllers/hello_controller.js
+++ app/frontend/controllers/hello_controller.js
@@ -2,6 +2,6 @@ import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
   connect() {
-    this.element.textContent = "Hello from Stimulus! 🙏"
+    this.element.textContent = "Hello 👋"
   }
 }
