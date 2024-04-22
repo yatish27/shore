c022b31375c0ebbb49f52a4b7fbd0909ecb47991
Yatish Mehta <yatish27@users.noreply.github.com>
2024-04-21 20:20:43 -0700

    Modified the subtitle

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @subtitle = "An opinionated and modern Ruby on Rails template with pragmatic defaults to start your next project 🚀"
   end
 end
