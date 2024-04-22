fcd36a063f9978b7b8be3b6bbe9686c881ba37cc
Yatish Mehta <yatish27@users.noreply.github.com>
2024-04-21 21:05:58 -0700

    Added subtitle in ERB

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
   def index
-    @subtitle = "An opinionated and modern Ruby on Rails template with pragmatic defaults to start your next project 🚀"
   end
 end
