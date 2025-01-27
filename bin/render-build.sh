933bb70bb10c51751df145b3d72d41b4acdbf210
Yatish Mehta <yatish27@users.noreply.github.com>
2025-01-26 17:57:17 -0800

    Removed bundle exec rails assets:clean from render-build.sh

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,7 +4,7 @@ set -o errexit
 
 bundle install
 bundle exec rails assets:precompile
-bundle exec rails assets:clean
+
 
 # If you're using a Free instance type, you need to
 # perform database migrations in the build command.
