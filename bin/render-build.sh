0d325f89367462be7b5a0078aa68e200810c13cf
Yatish Mehta <yatish27@users.noreply.github.com>
2025-01-26 17:30:54 -0800

    Added support for render deployment

---
+++ bin/render-build.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+bundle install
+bundle exec rails assets:precompile
+bundle exec rails assets:clean
+
+# If you're using a Free instance type, you need to
+# perform database migrations in the build command.
+# Uncomment the following line:
+
+bundle exec rails db:migrate
