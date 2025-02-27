1e1aab6490fe7a968751407fe0e7684033ef6e13
Yatish Mehta <yatish27@users.noreply.github.com>
2025-02-26 21:22:16 -0800

    Reset database schema to initial state

--- db/schema.rb
+++ db/schema.rb
@@ -10,48 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_02_26_062844) do
+ActiveRecord::Schema[8.0].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
-  create_table "object_types", force: :cascade do |t|
-    t.string "type_id", null: false
-    t.jsonb "definition", default: {}
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "deleted_at"
-    t.index ["deleted_at"], name: "index_object_types_on_deleted_at"
-    t.index ["type_id"], name: "index_object_types_on_type_id", unique: true
-  end
-
-  create_table "resources", force: :cascade do |t|
-    t.string "object_type", null: false
-    t.string "object_id", null: false
-    t.jsonb "meta", default: {}
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "deleted_at"
-    t.index ["deleted_at"], name: "index_resources_on_deleted_at"
-    t.index ["object_type", "object_id"], name: "index_resources_on_object_type_and_object_id", unique: true
-  end
-
-  create_table "warrants", force: :cascade do |t|
-    t.string "object_type", null: false
-    t.string "object_id", null: false
-    t.string "relation", null: false
-    t.string "subject_type", null: false
-    t.string "subject_id", null: false
-    t.string "subject_relation", default: ""
-    t.text "policy", default: "", null: false
-    t.string "policy_hash", default: "", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.datetime "deleted_at"
-    t.index ["deleted_at"], name: "index_warrants_on_deleted_at"
-    t.index ["object_type", "object_id", "relation", "subject_type", "subject_id", "subject_relation", "policy_hash"], name: "index_warrants_on_unique_constraint", unique: true
-  end
-
-  create_table "wookies", id: :string, force: :cascade do |t|
-    t.datetime "created_at", null: false
-  end
 end
