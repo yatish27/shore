e7b8ed374dbd31faff516e472352c80ef093bf1e
Yatish Mehta <yatish27@users.noreply.github.com>
2025-03-30 11:46:01 -0700

    Added support for inertiaJS along with SSR

--- config/initializers/inertia_rails.rb
+++ config/initializers/inertia_rails.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 InertiaRails.configure do |config|
-  config.ssr_enabled = ENV.fetch("INERTIA_SSR_ENABLED", false)
+  config.ssr_enabled = ENV.fetch("INERTIA_SSR_ENABLED", true)
   config.ssr_url = ENV.fetch("INERTIA_SSR_URL", "http://localhost:13714")
   config.version = ViteRuby.digest
 end
